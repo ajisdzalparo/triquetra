c8bbd4fd0609538caf6b1610bb60444f6aad053d
Ajis Dzalparo <ajisdzalparo22@gmail.com>
2025-04-29 11:20:37 +0700

    update button

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -203,11 +203,11 @@ const Home = () => {
 
           <InfoSection items={infoItems} />
           <div className="flex flex-col items-center">
-            <Button variant="default" className="w-full md:w-[300px] mt-5">
-              <a href="/pdf/Triquetra Converge360 Platform.pdf" target="_blank">
+            <a href="/pdf/Triquetra Converge360 Platform.pdf" target="_blank">
+              <Button variant="default" className="w-full md:w-[300px] mt-5">
                 Download Solution Document
-              </a>
-            </Button>
+              </Button>
+            </a>
           </div>
         </motion.div>
       </section>
